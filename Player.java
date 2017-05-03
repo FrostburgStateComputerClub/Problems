e5897c878d4d633a1b31f57aa0089c258a74fd07
mdameron0 <mdameron0@frostburg.edu>
2017-05-03 17:50:53 -0400

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import java.io.File;
+
+/**
+ *
+ * @author mdameron0
+ */
+public class Player extends ObjectClass{
+    public Player (){
+        super(new File("C:\\Users\\mdameron0\\Desktop\\balls.png"), "player");
+        
+    }
+}
